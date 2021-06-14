580e3a5668afbfed11af7ace0f645ef71ce9abd8
Soumya Padhi <spadhi@gmail.com>
2021-06-14 21:47:05 +0100

    Upto Chapter 2

---
+++ src/components/learning-examples/FirstComponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class FirstComponent extends Component {
+    render() {
+        return (
+            <div className="FirstComponent">
+                My FirstComponent from component file
+            </div>
+        );
+    }
+}
+
+export default FirstComponent
---
+++ src/components/learning-examples/ThirdComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ThirdComponent() {
+    return (
+        <div className="ThirdComponent">
+            My ThirdComponent from function
+        </div>
+    );
+}
+
+export default ThirdComponent
